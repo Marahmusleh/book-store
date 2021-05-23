02a6d9282a8094271933cc8836fa61f2a03f5a63
marahmusleh <marahaltarefe18@gmail.com>
2021-05-23 23:18:09 +0300

    finishing

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let username = prompt("Please Enter Your Name","");
+alert("WELCOME,"+username+"!");
+console.log(username);
+let userage = prompt("What is your age?");
+console.log(userage);
+let userbook = prompt("Enter your email to join our community of readers,writers,bookstore!")
+
+alert("You're Subscried,Thank you! Every purchase you make supports us!")
+console.log(userbook);
+let favbook = prompt ("What is your favourite book?")
+alert("Great Choice! Search for it now!")
+console.log(favbook);
+
+alert('Your name is'+ username +' your age is'+ userage +'your email is' + userbook +'and you favourite book is' + favbook)
